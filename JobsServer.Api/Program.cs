92ee798317dfcf34eeb73946f6b7378d8fb6fab8
citizen-dror <reshef.dror@gmail.com>
2025-03-02 18:11:55 +0200

    JsonSerializerOptions ReferenceHandler.IgnoreCycles for simple json responce

--- JobsServer.Api/Program.cs
+++ JobsServer.Api/Program.cs
@@ -15,7 +15,7 @@ builder.Logging.AddConsole();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     });
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
